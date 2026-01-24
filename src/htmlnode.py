44707de3eed419070949f416ffeb591a56c48251
jLun4rd1 <joaoclunardi@gmail.com>
2026-01-24 11:25:59 -0300

    :hammer: fix - add leading space so to separate attributes from tags when building html string and fix tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -14,9 +14,9 @@ class HTMLNode():
         final_string = []
         for key, value in self.props.items():
             final_string.append(f'{key}="{value}"')
-        return " ".join(final_string)
+        return " " + " ".join(final_string)
 
     def __repr__(self):
         return f"""
-            HTMLNode(\n\ttag: {self.tag}\n\tvalue: {self.value}\n\tchildren: {self.children}\n\tprops: {self.props_to_html}\n)
+            HTMLNode(\n\ttag: {self.tag}\n\tvalue: {self.value}\n\tchildren: {self.children}\n\tprops: {self.props_to_html()}\n)
         """
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -5,13 +5,13 @@ from htmlnode import HTMLNode
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
         node = HTMLNode("a", "click here", None, {"href": "https://www.google.com"})
-        expected = 'href="https://www.google.com"'
+        expected = ' href="https://www.google.com"'
         result = node.props_to_html()
         self.assertEqual(expected, result)
 
     def test_props_to_html_multiple_attributes(self):
         node = HTMLNode("a", "click here", None, {"href": "https://www.google.com", "target": "_blank"})
-        expected = 'href="https://www.google.com" target="_blank"'
+        expected = ' href="https://www.google.com" target="_blank"'
         result = node.props_to_html()
         self.assertEqual(expected, result)
 
