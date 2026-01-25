132d37bd6eab38d6984dc016260b2a5ab4d0dea5
jLun4rd1 <joaoclunardi@gmail.com>
2026-01-25 18:02:33 -0300

    :sparkles: feat - add markdown image and links extractors and tests!

---
+++ src/test_extract_markdown_images.py
@@ -0,0 +1,35 @@
+import unittest
+from extract_markdown_images import extract_markdown_images
+
+
+class TestExtractMarkdownImages(unittest.TestCase):
+    def test_extract_markdown_images_missing_exclamation_point(self):
+        matches = extract_markdown_images(
+            "This is text with a link [to google](https://www.google.com)"
+        )
+        self.assertListEqual([], matches)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_images_multiple_images(self):
+        matches = extract_markdown_images(
+            """
+            This is text with image A ![image A](https://i.imgur.com/zjjcJKZ.png)
+            and image B ![image B](https://imgur.com/gallery/wild-regex-bug-appeared-Alk0gcf#/t/regex)
+            """
+        )
+        self.assertListEqual(
+            [
+                ("image A", "https://i.imgur.com/zjjcJKZ.png"),
+                ("image B", "https://imgur.com/gallery/wild-regex-bug-appeared-Alk0gcf#/t/regex")
+            ],
+            matches
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/test_extract_markdown_links.py
@@ -0,0 +1,35 @@
+import unittest
+from extract_markdown_links import extract_markdown_links
+
+
+class TestExtractMarkdownImages(unittest.TestCase):
+    def test_extract_markdown_links_with_exclamation_point(self):
+        matches = extract_markdown_links(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to google](https://www.google.com)"
+        )
+        self.assertListEqual([("to google", "https://www.google.com")], matches)
+
+    def test_extract_markdown_links_multiple_images(self):
+        matches = extract_markdown_links(
+            """
+            This is text with a link [to google](https://www.google.com)
+            and [to youtube](https://www.youtube.com)
+            """
+        )
+        self.assertListEqual(
+            [
+                ("to google", "https://www.google.com"),
+                ("to youtube", "https://www.youtube.com")
+            ],
+            matches
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
