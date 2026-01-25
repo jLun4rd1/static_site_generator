11933335b177a00042ffbe4abebf61bda912cb4c
jLun4rd1 <joaoclunardi@gmail.com>
2026-01-25 15:55:26 -0300

    :truck: move - moved function text_node_to_html_node() to a separate file and renamed test file for it

--- src/test_text_node_to_html_node.py
+++ src/test_text_node_to_html_node.py
@@ -1,30 +1,30 @@
 import unittest
 from leafnode import LeafNode
 from textnode import TextNode, TextType
-from main import text_node_to_html_node
+from text_node_to_html_node import text_node_to_html_node
 
 
 class TestMainFunctions(unittest.TestCase):
-    def test_text_node_to_html_node_text(self):
+    def test_text(self):
         node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
 
-    def test_text_node_to_html_node_bold(self):
+    def test_bold(self):
         node = TextNode("This is a bold text node", TextType.BOLD)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "This is a bold text node")
 
-    def test_text_node_to_html_node_image(self):
+    def test_image(self):
         node = TextNode("This is an image text node", TextType.IMAGE, "public/my_image.png")
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "img")
         self.assertEqual(html_node.value, None)
         self.assertEqual(html_node.props, {"src": "public/my_image.png", "alt": "This is an image text node"})
 
-    def test_text_node_to_html_node_link(self):
+    def test_link(self):
         node = TextNode("Click to go to Google!", TextType.LINK, "https://www.google.com")
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "a")
