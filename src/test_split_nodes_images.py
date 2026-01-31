55d71a2ef9eb38f03ca4174c40f30295f98d00f3
jLun4rd1 <joaoclunardi@gmail.com>
2026-01-31 11:17:46 -0300

    :sparkles: feat - add both split_noes_images.py and a file with tests for it

---
+++ src/test_split_nodes_images.py
@@ -0,0 +1,100 @@
+import unittest
+from textnode import TextNode, TextType
+from split_nodes_images import split_nodes_images
+
+
+class TestSplitNodesImages(unittest.TestCase):
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_images([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_starting_image(self):
+        node = TextNode(
+            "![image](https://i.imgur.com/zjjcJKZ.png) What about now that I started with an image?",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_images([node])
+        self.assertListEqual(
+            [
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" What about now that I started with an image?", TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+    def test_split_ending_image(self):
+        node = TextNode(
+            "What about now that I ended with an image? ![image](https://i.imgur.com/zjjcJKZ.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_images([node])
+        self.assertListEqual(
+            [
+                TextNode("What about now that I ended with an image? ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            ],
+            new_nodes,
+        )
+
+    def test_split_image_with_leading_and_trailing_space(self):
+        node = TextNode(
+            " ![image](https://i.imgur.com/zjjcJKZ.png) ",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_images([node])
+        self.assertListEqual(
+            [
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            ],
+            new_nodes,
+        )
+
+    def test_spilt_two_images_with_starting_second(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) ![second image](https://i.imgur.com/3elNhQu.png) and it ends",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_images([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+                TextNode(" and it ends", TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+    def test_split_two_starting_images(self):
+        node = TextNode(
+            "![image](https://i.imgur.com/zjjcJKZ.png) this is text with a ![second image](https://i.imgur.com/3elNhQu.png) and it ends",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_images([node])
+        self.assertListEqual(
+            [
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" this is text with a ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+                TextNode(" and it ends", TextType.TEXT),
+            ],
+            new_nodes,
+        )
