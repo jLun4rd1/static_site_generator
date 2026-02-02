1eb68827b13dd4499d2d83397f124be1f3b73207
jLun4rd1 <joaoclunardi@gmail.com>
2026-02-01 22:50:41 -0300

    :sparkles: feat - create function to generate pages recursively given a content path and a destination path

---
+++ src/generate_pages_recursive.py
@@ -0,0 +1,15 @@
+import os
+from generate_page import generate_page
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    if os.path.isfile(dir_path_content):
+        html_dest_dir_path = dest_dir_path.replace(".md", ".html")
+        generate_page(dir_path_content, template_path, html_dest_dir_path)
+        return
+    else:
+        all_files = os.listdir(dir_path_content)
+        for file in all_files:
+            updated_dir_path_content = os.path.join(dir_path_content, file)
+            updated_dest_dir_path = os.path.join(dest_dir_path, file)
+            generate_pages_recursive(updated_dir_path_content, template_path, updated_dest_dir_path)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from copy_to_public_folder import copy_to_public_folder
-from generate_page import generate_page
+from generate_pages_recursive import generate_pages_recursive
 
 def main():
     copy_to_public_folder()
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content/", "template.html", "public/")
 
 if __name__ == "__main__":
     main()
